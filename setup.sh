fb2b1e635a1a319771e3a5e9a272ce3b6e566eed
151044 <yhchowai@connect.ust.hk>
2026-01-21 14:04:23 +0800

    feat: Add Google Noto Fonts for emojis
    Signed-off-by: 151044 <yhchowai@connect.ust.hk>

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# The Python venv
 python -m venv piano-venv
 source piano-venv/bin/activate
-pip install numpy pillow tensorflow[and-cuda] sounddevice tf_keras opencv-python
+pip install numpy pillow tensorflow sounddevice tf_keras opencv-python
+
+# The PortAudio library
+cd portaudio || return
+./configure
+make && make install
