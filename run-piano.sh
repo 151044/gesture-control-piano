bb472b5d850e166658f1db31f8cfa5defb3623cd
151044 <yhchowai@connect.ust.hk>
2026-01-21 15:21:40 +0800

    fix: Incorrect preload path
    Signed-off-by: 151044 <yhchowai@connect.ust.hk>

--- run-piano.sh
+++ run-piano.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source piano-venv/bin/activate
-LD_LIBRARY_PATH=/home/data/gesture-control-piano/lib:$LD_LIBRARY_PATH python main.py
+LD_LIBRARY_PATH=/home/data/gesture-control-piano/lib/lib:$LD_LIBRARY_PATH python main.py
